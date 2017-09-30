973f9598d6108cff81bec749e15124d9fc564d2e
Abialbon Paul <dr.paulnotes@gmail.com>
2017-09-30 20:37:53 +0000

    Completed

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const app       = express();
 const path      = require("path");
 const PORT      = process.env.PORT || 3000;
 const IP        = process.env.IP || '127.0.0.1';
+const multer    = require("multer");
+const upload    = multer();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/file', (req, res) => {
-   res.send('The file upload path was hit!'); 
+app.post('/file', upload.single('userfile'), (req, res) => {
+    let response = {
+        size: req.file.size
+    }
+    res.json(response); 
 });
 
 app.listen(process.env.PORT, process.env.IP, () => {
